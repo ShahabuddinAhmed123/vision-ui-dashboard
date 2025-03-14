1450dd2231447d53ae35762836b3163ab0650e55
ShahabuddinAhmed123 <shahabtanolo123@gmail.com>
2025-03-15 00:22:42 +0500

    added arabic language for /rtl page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import AuthProvider from "@/context/AuthContext";
 import { useState, useEffect } from "react";
+import Head from "next/head";
 import "./globals.css";
-import "./i18n"
+import "./i18n";
 import { usePathname } from "next/navigation";
 
 export default function RootLayout({
@@ -34,7 +35,6 @@ export default function RootLayout({
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-
   const language = pathname.includes("/rtl") ? "ar" : "en";
 
   useEffect(() => {
@@ -43,6 +43,12 @@ export default function RootLayout({
 
   return (
     <html lang={language}>
+      <Head>
+        <title>Vision ui dashboard</title>
+        <meta name="description" content="This is my application description." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="flex">
         <Navbar openSidebar={openSidebar} handleOpenSidebar={handleOpenSidebar} />
         {openSidebar && <Sidebar onClick={handleCloseSidebar} />}
